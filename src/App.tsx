e095f846abfeeb90bcb14dac4a061d4c18a8f57c
vikalpis <149229170+vikalpis@users.noreply.github.com>
2025-08-06 13:27:34 +0530

    Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import { Button } from './component/ui/Button'
+import { Card } from './component/ui/Card'
 import {PlusIcon} from "./icons/PlusIcon"
 import { ShareIcon } from './icons/ShareIcon'
 
@@ -8,8 +9,22 @@ function App() {
 
   return (
     <>
+    <div className='p-2'>
+      <div className='flex justify-end gap-2'>
       <Button variants='primary' text='Add Content' size='sm' startIcon={<PlusIcon size='lg'/>}/>
       <Button variants='secondary' text='Share Brain' size='md' startIcon={<ShareIcon size='md'/>}/>
+      
+     </div>
+
+    <div className='flex gap-4'>
+      <Card title='new' link='https://x.com/benawad/status/1952436349677551758' type='twitter'/>
+
+      <Card title='second' link='https://www.youtube.com/watch?v=p6ca7gq5H70&list=RDp6ca7gq5H70&start_radio=1' type='youtube'/>
+
+    </div>
+    </div>
+    
+      
     </>
   )
 }
---
+++ src/component/ui/Card.tsx
@@ -0,0 +1,42 @@
+import { ShareIcon } from "../../icons/ShareIcon";
+
+interface Cradprops{
+    title : string,
+    link: string,
+    type: "twitter" | "youtube" 
+}
+
+export function Card ({title, link, type}: Cradprops){
+    return <div>
+        <div className="bg-white border border-gray-200 max-w-86 rounded-md min-h-40 min-w-72">
+            <div className="flex justify-between">
+                <div className="flex items-center p-2">
+                    <ShareIcon size="md"/>
+                    <div className="pl-2">
+                        {title}
+                    </div>
+                </div>
+                <div className="flex items-center text-gray-500">
+                    <div className="pr-2">
+                        <a href={link} target="_blank">
+                        <ShareIcon size="md"/></a>
+                    </div>
+                    <div className="pr-2">
+                        <ShareIcon size="md"/>
+
+                    </div>
+                </div>
+                </div>
+                <div className=" p-2">
+                    {type === "youtube" &&  <iframe className="w-full p-4 rounded-md" src= {link.replace("watch", "embed")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>}
+                 
+                    {type ==="twitter" && <blockquote className="twitter-tweet">
+                     <a href={link.replace("x", "twitter")}></a> 
+                    </blockquote>}
+                    
+                </div>
+            
+        </div>
+
+    </div>
+}
