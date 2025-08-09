2a0fb5c4e3eb02ad7dfc2290ad48ce1d5b957b7a
vikalpis <149229170+vikalpis@users.noreply.github.com>
2025-08-09 21:50:24 +0530

    .

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Button } from "../component/ui/Button";
 import { Input } from "../component/ui/Input";
+import axios from "axios";
+import { BACKEND_URL } from "../component/ui/config";
+import { useNavigate } from "react-router-dom";
 
 export function Login(){
+    const usernameRef = useRef<HTMLInputElement>(null)
+    const passwordRef = useRef<HTMLInputElement>(null)
     const [disabled, setDisabled] = useState(false)
+    const navigate = useNavigate();
+    async function Login(){
+        const username = usernameRef.current?.value;
+        const password = passwordRef.current?.value;
+        const response = await axios.post(`${BACKEND_URL}/api/v1/user/login`, {
+            username,
+            password
+        });
+       const jwt = response.data.token;
+       localStorage.setItem("token", jwt)
+
+       //  TODO: redirect to it's dashboard
+        navigate("/dashboard");
+
+    }
     return <div>
     <div className="h-screen w-screen bg-black/76 flex justify-center items-center ">
     
@@ -11,11 +31,12 @@ export function Login(){
     <div className=" flex justify-center text-blue-400">
         Login
     </div>
-        <Input placeholder="Username" />
-        <Input placeholder="Password"/>
+        <Input ref={usernameRef} placeholder="Username" />
+        <Input ref={passwordRef} placeholder="Password"/>
 
         <div className="flex  justify-center" >
         <Button variants="primary" text="Submit" size="md" fullwidth={true} loading={disabled} onClick={()=>{
+            Login()
             setDisabled(e=>!e)
         }}/>
    
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -3,21 +3,23 @@ import { Button } from "../component/ui/Button";
 import { Input } from "../component/ui/Input";
 import axios from "axios";
 import { BACKEND_URL } from "../component/ui/config";
+import { useNavigate } from "react-router-dom";
 
 export function Signup(){
     const usernameRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
-
+    const navigate = useNavigate()
     const [disabled, setDisabled] = useState(false)
 
     async function Signup(){
         const username = usernameRef.current?.value;
         const password = passwordRef.current?.value;
-        await axios.post(`${BACKEND_URL}/api/v1/user/Signup`, {
+        await axios.post(`${BACKEND_URL}/api/v1/user/signup`, {
             username,
             password
         });
         alert("you are signed UP!")
+        navigate("/login")
     }
     return <div>
     <div className="h-screen w-screen bg-black/76 flex justify-center items-center ">
