fe55a0744cf9e1d1b7cc3780a87ae78f99d14468
vikalpis <149229170+vikalpis@users.noreply.github.com>
2025-08-11 12:42:33 +0530

    ..

--- src/component/ui/Sidebar.tsx
+++ src/component/ui/Sidebar.tsx
@@ -4,19 +4,20 @@ import { TwitterIcon } from "../../icons/TwitterIcon";
 import { YoutubeIcon } from "../../icons/YoutubeIcon";
 import { SidebarItem } from "./SidebarItem";
 
-export function Sidebar(){
-    return <div className="h-screen bg-white border-r w-76 fixed left-0 top-0">
-       <div className="flex items-center p-2 m-2 cursor-pointer  ">
-        <LogoIcon size="xl"/> 
-        <div className="p-2 font-bold text-2xl tracking-tight ">
-            SecondBrain
+export function Sidebar() {
+    return (
+        <div className="hidden  lg:block transition-all duration-300 sm:h-screen bg-white border-r sm:w-76 fixed left-0 top-0">
+            <div className=" hidden sm:flex items-center p-2 m-2 cursor-pointer">
+                <LogoIcon size="xl" /> 
+                <div className="p-2 font-bold text-2xl tracking-tight">
+                    SecondBrain
+                </div>
+            </div> 
+            <div className="text-gray-600">
+                <SidebarItem icon={<TwitterIcon />} text="Twitter" />
+                <SidebarItem icon={<YoutubeIcon />} text="Youtube" />
+                <SidebarItem icon={<DocumentIcon />} text="Document" />
             </div>
-        </div> 
-        <div className="text-gray-600">
-        <SidebarItem icon={<TwitterIcon/>} text='Twitter' />
-        <SidebarItem icon={<YoutubeIcon/>} text='Youtube'/>
-        <SidebarItem icon={<DocumentIcon/>} text='Document'/>
-
         </div>
-    </div>
+    );
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,9 @@
-
 import { useState } from 'react'
 import '../App.css'
 import { Button } from '../component/ui/Button'
 import { Card } from '../component/ui/Card'
 import { CreateContentModel } from '../component/ui/CreateContentModel'
-import {PlusIcon} from "../icons/PlusIcon"
+import { PlusIcon } from "../icons/PlusIcon"
 import { ShareIcon } from '../icons/ShareIcon'
 import { Sidebar } from '../component/ui/Sidebar'
 import { useContent } from '../hooks/useContent'
@@ -12,41 +11,48 @@ import { useContent } from '../hooks/useContent'
 function Dashboard() {
   const [openModel, setOpenModel] = useState(false)
   const contents = useContent()
-  return <div>
 
-    <Sidebar/>
-  
-  <div className='p-4 ml-72 min-h-screen bg-gray-200 border-2'> 
-        
-        <CreateContentModel open={openModel} closeModel={()=>{
-           setOpenModel(e => !e)
-        }
-         } />
-      <div className='flex justify-end gap-2' >
-      <Button variants='primary' text='Add Content' size='sm' startIcon={<PlusIcon size='lg'/>} onClick={()=>{
-        setOpenModel(e=>!e)
-      }}/>
-      <Button variants='secondary' text='Share Brain' size='md' startIcon={<ShareIcon size='md'/>}/>
-      
-     </div>
+  return (
+    <div className="flex flex-col md:flex-row">
+      <Sidebar />
 
-    <div className='flex gap-4'>
-      
-      {contents.map(({type, link, title})=>
-      <Card
-      title= {title}
-      link = {link}
-      type={type}/>
-      )}
-      
+      {/* Main content */}
+      <div className="p-4 md:ml-72 min-h-screen bg-gray-200 border-2 w-full">
+        <CreateContentModel
+          open={openModel}
+          closeModel={() => {
+            setOpenModel(e => !e)
+          }}
+        />
 
-      
+        {/* Top buttons */}
+        <div className="flex flex-col sm:flex-row justify-end gap-2 mb-4">
+          <Button
+            variants="primary"
+            text="Add Content"
+            size="sm"
+            startIcon={<PlusIcon size="lg" />}
+            onClick={() => {
+              setOpenModel(e => !e)
+            }}
+          />
+          <Button
+            variants="secondary"
+            text="Share Brain"
+            size="md"
+            startIcon={<ShareIcon size="md" />}
+          />
+        </div>
+
+        {/* Cards grid */}
+        <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          {contents.map(({ type, link, title }) => (
+            <Card key={link} title={title} link={link} type={type} />
+          ))}
+        </div>
+      </div>
     </div>
-   
-
-     
-    </div>
-    </div>
+  )
 }
 
 export default Dashboard
