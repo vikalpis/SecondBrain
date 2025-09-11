0bdea059caa9643ce272bc9ecf3a8f7fb3af61b8
vikalpis <149229170+vikalpis@users.noreply.github.com>
2025-09-11 23:35:20 +0530

    1.5.6

--- src/validation/schema.ts
+++ src/validation/schema.ts
@@ -2,7 +2,12 @@ import { z } from "zod";
 
 export const signupSchema = z.object({
     username : z.string().min(4, {message :"username should contain atleast 4 letters"}),
-    password : z.string().min(10).max(20).refine((val)=> /[A-Z]/.test(val), {message: "paassword must include capital letters "}).refine((val)=> /[a-z]/.test(val), {
+    password : z.string().min(10,
+        {message : "Password should contain atleast 10 charecters"})
+        .max(20 ,{message : "Password should not contain more than 20 charecters"})
+        .refine((val)=> /[A-Z]/.test(val),
+        {message: "paassword must include capital letters "})
+        .refine((val)=> /[a-z]/.test(val), {
         message: "password must include small letters"
     })
 })
